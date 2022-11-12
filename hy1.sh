c9286b087a5d0a9a929e68495f7e5213304b38d2
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-11-12 07:07:44 +0000

    Update hy1.sh

--- hy1.sh
+++ hy1.sh
@@ -658,7 +658,7 @@ hysteriashare
 
 changeserv(){
 green "hysteria配置变更选择如下:"
-readp "1. 切换IPV4/IPV6出站优先级\n2. 切换传输协议类型\n3. 切换证书类型(支持/root/ygkkkca路径上传自定义证书)\n4. 更换验证密码\n5. 变更端口：单端口、多端口、范围端口（将重置所有端口重新分配）\n6. 返回上层\n请选择：" choose
+readp "1. 切换IPV4/IPV6出站优先级\n2. 切换传输协议类型\n3. 切换证书类型(支持/root/ygkkkca路径上传自定义证书)\n4. 更换验证密码\n5. 变更端口（将重置所有端口）：单端口、多端口、范围端口\n6. 返回上层\n请选择：" choose
 if [ $choose == "1" ];then
 changeip
 elif [ $choose == "2" ];then
