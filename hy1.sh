607363b49fb266d6df33848e54fbc3fc344a9978
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-11-12 07:15:43 +0000

    Update hy1.sh

--- hy1.sh
+++ hy1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-hyygV="22.10.27 V 4.0"
+hyygV="22.11.12 V 5.0"
 remoteV=`wget -qO- https://gitlab.com/rwkgyg/hysteria-yg/raw/main/hysteria.sh | sed  -n 2p | cut -d '"' -f 2`
 chmod +x /root/hysteria.sh 
 red='\033[0;31m'
