e2d9a24b5fce64a712a32bcf2341134204a7bb34
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2023-01-19 01:22:11 +0000

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-hyygV="22.12.9 V 5.7"
+hyygV="23.1.19 V 5.8"
 remoteV=`wget -qO- https://gitlab.com/rwkgyg/hysteria-yg/raw/main/hysteria.sh | sed  -n 2p | cut -d '"' -f 2`
 chmod +x /root/hysteria.sh 
 red='\033[0;31m'
