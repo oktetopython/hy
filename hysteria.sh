66696c3ff5755e9d7a3d434f3c87f8dbb651537f
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2023-05-02 23:13:28 +0000

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -141,10 +141,6 @@ green "已安装hysteria，重装请先执行卸载功能" && exit
 fi
 if [[ $release = Centos ]]; then
 if [[ ${vsid} =~ 8 ]]; then
-cd /etc/yum.repos.d/ && mkdir backup && mv *repo backup/ 
-curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
-sed -i -e "s|mirrors.cloud.aliyuncs.com|mirrors.aliyun.com|g " /etc/yum.repos.d/CentOS-*
-sed -i -e "s|releasever|releasever-stream|g" /etc/yum.repos.d/CentOS-*
 yum clean all && yum makecache
 fi
 yum install epel-release -y
